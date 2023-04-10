efd4f8b73e00c56a590d0b03c3d0a4641d3b4140
Neeraj Bisht <neerajb@BLRETV-MF62J0.local>
2023-04-10 14:43:27 +0530

    Handling deployment case in policy_recommnedation_registrar.go

--- internal/controller/policyrecommendation_registrar.go
+++ internal/controller/policyrecommendation_registrar.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	argov1alpha1 "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	ottoscaleriov1alpha1 "github.com/flipkart-incubator/ottoscalr/api/v1alpha1"
+	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -27,50 +28,63 @@ type PolicyRecommendationRegistrar struct {
 	Scheme *runtime.Scheme
 }
 
-func (r *PolicyRecommendationRegistrar) Reconcile(ctx context.Context,
-	request ctrl.Request) (ctrl.Result, error) {
-
+func (r *PolicyRecommendationRegistrar) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
+	log = log.WithValues("request", request)
 
-	// Fetch the Deployment or Rollout instance
-	//TODO handle the case for deployment
-	instance := argov1alpha1.Rollout{}
-	err := r.Client.Get(ctx, request.NamespacedName, &instance)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			log.Info("Object not found, could have been deleted after reconcile request")
-			// Object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			return reconcile.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		log.Error(err, "Error reading the object - requeue the request")
+	// Check if Rollout exists
+	rollout := argov1alpha1.Rollout{}
+	err := r.Client.Get(ctx, request.NamespacedName, &rollout)
+	if err == nil {
+		// Rollout exists, create policy recommendation
+		return r.createPolicyRecommendation(ctx, &rollout, log)
+	}
+
+	if !errors.IsNotFound(err) {
+		// Error occurred
+		log.Error(err, "Failed to get Rollout. Requeue the request")
 		return reconcile.Result{}, err
 	}
 
+	// Check if Deployment exists
+	deployment := appsv1.Deployment{}
+	err = r.Client.Get(ctx, request.NamespacedName, &deployment)
+	if err == nil {
+		// Deployment exists, create policy recommendation
+		return r.createPolicyRecommendation(ctx, &deployment, log)
+	}
+
+	if !errors.IsNotFound(err) {
+		log.Error(err, "Failed to get Deployment. Requeue the request")
+		return reconcile.Result{}, err
+	}
+
+	log.Info("Rollout or Deployment not found. It could have been deleted.")
+	return reconcile.Result{}, nil
+}
+
+func (r *PolicyRecommendationRegistrar) createPolicyRecommendation(ctx context.Context,
+	instance client.Object,
+	log logr.Logger) (ctrl.Result, error) {
 	// Check if a PolicyRecommendation object already exists
 	policy := &ottoscaleriov1alpha1.PolicyRecommendation{}
-	err = r.Client.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, policy)
+	err := r.Client.Get(ctx, types.NamespacedName{Name: instance.GetName(), Namespace: instance.GetNamespace()}, policy)
 	if err == nil {
 		log.Info("PolicyRecommendation object already exists")
-		// PolicyRecommendation already exists, so we don't need to create one
 		return ctrl.Result{}, nil
 	} else if !errors.IsNotFound(err) {
-		// Error reading the object - requeue the request.
 		log.Error(err, "Error reading the object - requeue the request")
 		return ctrl.Result{}, err
 	}
 
 	log.Info("Creating a new PolicyRecommendation object")
 
-	// Create a new PolicyRecommendation object
 	newPolicyRecommendation := &ottoscaleriov1alpha1.PolicyRecommendation{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name,
-			Namespace: instance.Namespace,
+			Name:      instance.GetName(),
+			Namespace: instance.GetNamespace(),
 			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(&instance, instance.GroupVersionKind()),
+				*metav1.NewControllerRef(instance, instance.GetObjectKind().GroupVersionKind()),
 			},
 		},
 		Spec: ottoscaleriov1alpha1.PolicyRecommendationSpec{
@@ -99,13 +113,12 @@ func (r *PolicyRecommendationRegistrar) SetupWithManager(mgr ctrl.Manager) error
 		},
 	}
 
-	enqueueFunc := func(a client.Object) []reconcile.Request {
-		return []reconcile.Request{{NamespacedName: types.NamespacedName{Name: a.GetName(), Namespace: a.GetNamespace()}}}
+	enqueueFunc := func(obj client.Object) []reconcile.Request {
+		return []reconcile.Request{{NamespacedName: types.NamespacedName{Name: obj.GetName(),
+			Namespace: obj.GetNamespace()}}}
 	}
 
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&argov1alpha1.Rollout{}).
-		For(&appsv1.Deployment{}).
 		Watches(
 			&source.Kind{Type: &argov1alpha1.Rollout{}},
 			handler.EnqueueRequestsFromMapFunc(enqueueFunc),
--- internal/controller/policyrecommendation_registrar_test.go
+++ internal/controller/policyrecommendation_registrar_test.go
@@ -2,20 +2,21 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	rolloutv1alpha1 "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	ottoscaleriov1alpha1 "github.com/flipkart-incubator/ottoscalr/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
 )
 
-func TestPolicyRecommendationRegistrationController(t *testing.T) {
+func TestPolicyRecommendationRegistrar_Rollout(t *testing.T) {
 
 	// Create a new scheme and register the types with it
 	scheme := runtime.NewScheme()
@@ -72,9 +73,75 @@ func TestPolicyRecommendationRegistrationController(t *testing.T) {
 	result, err = controller.Reconcile(context.Background(), request)
 	assert.NoError(t, err)
 	assert.True(t, result.Requeue == false)
-
 	policies := &ottoscaleriov1alpha1.PolicyRecommendationList{}
 	err = fakeClient.List(context.Background(), policies)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(policies.Items))
+	fmt.Println(policies.Items[0].OwnerReferences)
+
+}
+
+func TestPolicyRecommendationRegistrar_Deployment(t *testing.T) {
+
+	// Create a new scheme and register the types with it
+	scheme := runtime.NewScheme()
+	err := clientgoscheme.AddToScheme(scheme)
+	assert.NoError(t, err)
+	err = rolloutv1alpha1.AddToScheme(scheme)
+	assert.NoError(t, err)
+	err = ottoscaleriov1alpha1.AddToScheme(scheme)
+	assert.NoError(t, err)
+
+	deployment := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-deployment",
+			Namespace: "my-namespace",
+		},
+	}
+
+	// Create a fake controller runtime client with the instance and policy
+	fakeClient := fake.NewClientBuilder().
+		WithScheme(scheme).
+		WithRuntimeObjects(deployment).
+		Build()
+
+	// Create the controller and inject the fake client
+	controller := &PolicyRecommendationRegistrar{
+		Client: fakeClient,
+	}
+
+	// Create the reconcile request
+	request := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "my-deployment",
+			Namespace: "my-namespace",
+		},
+	}
+
+	// Call the Reconcile method and verify that a new PolicyRecommendation was created
+	result, err := controller.Reconcile(context.Background(), request)
+	assert.NoError(t, err)
+	assert.True(t, result.Requeue == false)
+
+	foundPolicy := &ottoscaleriov1alpha1.PolicyRecommendation{}
+	err = fakeClient.Get(context.Background(), types.NamespacedName{Name: "my-deployment", Namespace: "my-namespace"}, foundPolicy)
+	assert.NoError(t, err)
+	assert.Equal(t, deployment.GetName(), foundPolicy.OwnerReferences[0].Name)
+	assert.Equal(t, deployment.Kind, foundPolicy.OwnerReferences[0].Kind)
+	assert.Equal(t, deployment.APIVersion, foundPolicy.OwnerReferences[0].APIVersion)
+
+	// Call the Reconcile method again and verify that a new PolicyRecommendation is not created
+	result, err = controller.Reconcile(context.Background(), request)
+	assert.NoError(t, err)
+	assert.True(t, result.Requeue == false)
+	policies := &ottoscaleriov1alpha1.PolicyRecommendationList{}
+	err = fakeClient.List(context.Background(), policies)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(policies.Items))
+	fmt.Println(policies.Items[0].OwnerReferences)
+
 }
