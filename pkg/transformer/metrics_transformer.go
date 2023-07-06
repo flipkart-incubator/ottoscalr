6d62fefeae53724ff6c867155004f0c6396b0d1d
deefreak <deepakgts2@gmail.com>
2023-07-06 19:41:57 +0530

    interpolate bug fix

--- pkg/transformer/metrics_transformer.go
+++ pkg/transformer/metrics_transformer.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/flipkart-incubator/ottoscalr/pkg/integration"
 	"github.com/flipkart-incubator/ottoscalr/pkg/metrics"
-	"github.com/go-logr/logr"
 	"sort"
 	"time"
 )
@@ -16,14 +15,12 @@ type OutlierInterval struct {
 
 type OutlierInterpolatorTransformer struct {
 	EventIntegration []integration.EventIntegration
-	logger           logr.Logger
 }
 
-func NewOutlierInterpolatorTransformer(eventIntegration []integration.EventIntegration, logger logr.Logger) (*OutlierInterpolatorTransformer, error) {
+func NewOutlierInterpolatorTransformer(eventIntegration []integration.EventIntegration) (*OutlierInterpolatorTransformer, error) {
 
 	return &OutlierInterpolatorTransformer{
 		EventIntegration: eventIntegration,
-		logger:           logger,
 	}, nil
 }
 
@@ -95,9 +92,8 @@ func (ot *OutlierInterpolatorTransformer) cleanOutliersAndInterpolate(dataPoints
 		newDataPoints = append(newDataPoints, dataPoint)
 	}
 	for _, interval := range intervals {
-		ot.logger.V(2).Info("Interpolating for interval: ", "start", interval.StartTime, "end", interval.EndTime)
 		startIndex := -1
-		endIndex := 0
+		endIndex := -1
 		for i := 0; i < len(newDataPoints); i++ {
 			if newDataPoints[i].Timestamp.After(interval.StartTime) && newDataPoints[i].Timestamp.Before(interval.EndTime) {
 				if startIndex == -1 {
@@ -105,6 +101,10 @@ func (ot *OutlierInterpolatorTransformer) cleanOutliersAndInterpolate(dataPoints
 				}
 				endIndex = i
 			}
+		}
+
+		if startIndex == -1 && endIndex == -1 {
+			continue
 		}
 
 		if endIndex >= len(newDataPoints)-1 {
